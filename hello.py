f2c407c6f5f4d805a07db9f511619ac856505257
Ryoichi Fandry <ryoichifandry@gmail.com>
2021-06-30 16:06:06 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+import ssaw
+from dotenv import dotenv_values
+from ssaw import interviews
+from ssaw.headquarters_schema import String
+
+config = dotenv_values(".env")
+
+client = ssaw.Client(url=config['SERVER_URL'], api_user=config['API_USER'], api_password=config['API_PASSWORD'])
+
+questionnaire = next(q for q in ssaw.QuestionnairesApi(client).get_list() if q.title == "Bokin-trano")
+statuses = ssaw.QuestionnairesApi(client, workspace="primary").statuses()
+print(statuses)
+interviews = ssaw.InterviewsApi(client, workspace="primary").get_list(questionnaire_id=questionnaire.questionnaire_id, status="APPROVEDBYHEADQUARTERS")
+for i in interviews:
+  print(i)
+  info = ssaw.InterviewsApi(client, workspace="primary").get_info(i.id)
+  for ligne in info:
+    if (ligne['Answer']):
+      print(ligne['VariableName'] + ': ' + ligne['Answer'])
+    else:
+      print(ligne['VariableName'] + ': ' + '- VIDE -')
